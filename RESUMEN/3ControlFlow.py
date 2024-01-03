9eee9cb19c90069001de8dfadef0b9a2f280aef7
gciceri <153392320+gciceri@users.noreply.github.com>
2024-01-03 00:33:15 -0300

    eliminar info loop

--- RESUMEN/3ControlFlow.py
+++ RESUMEN/3ControlFlow.py
@@ -88,7 +88,22 @@ else:
     elif opciones_registro == "N":
         print("Saliendo del programa...")
 print(user_pass)        
-       
+
+#otro eemplo
+lista={"giancarlo":[12],
+       "Tomas":[15]}
+
+new=input("nombre")
+if new in lista:
+    print("ya esta registrado")
+else:
+    registro=input("no esta registrado. desea registrarse S/N")
+    
+    if registro == 's' or registro=="S":
+        contrasena=input("escriba contrasena")     
+        print("se a registrado con exito")
+    elif registro != "n":
+        print("usuario no registrado")
 
     
     
